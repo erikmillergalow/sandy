5f46042c6170a9288d25123dcb72ff69b32b4e1a
Erik Miller-Galow <erikmillergalow@gmail.com>
2022-01-30 17:47:59 -0500

    Able to load images onto the main texture

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,9 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <../include/stb_image.h>
+
+#include <../include/shader.h>
+
 #include <iostream>
 
 void framebuffer_size_callback(GLFWwindow *window, int width, int height);
@@ -12,24 +16,25 @@ const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
 // initial canvas shader definition
-const char *vertexShaderSource = "#version 330 core\n"
-    "layout (location = 0) in vec3 aPos;\n"
-    "void main()\n"
-    "{\n"
-    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-    "}\0";
+// const char *vertexShaderSource = "#version 330 core\n"
+//     "layout (location = 0) in vec3 aPos;\n"
+//     "void main()\n"
+//     "{\n"
+//     "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+//     "}\0";
 
-// initial fragment shader source
-const char *fragmentShaderSource = "#version 330 core\n"
-    "out vec4 FragColor;\n"
-    "void main()\n"
-    "{\n"
-    "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
-    "}\n\0";
+// // initial fragment shader source
+// const char *fragmentShaderSource = "#version 330 core\n"
+//     "out vec4 FragColor;\n"
+//     "void main()\n"
+//     "{\n"
+//     "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+//     "}\n\0";
 
 int main()
 {
 	// glfw: initialize and configure
+    std::cout << "Starting..."  << std::endl;
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
@@ -39,6 +44,7 @@ int main()
 #endif
 
 	// glfw window creation
+    std::cout << "Creating window..."  << std::endl;
 	GLFWwindow *window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Sandy", NULL, NULL);
 	if (window == NULL)
 	{
@@ -47,119 +53,144 @@ int main()
 		return -1;
 	}
 	glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
 	// glad: load all OpenGL function pointers
+    std::cout << "Loading OpenGL function pointers..."  << std::endl;
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
 	{
 		std::cout << "Failed to initialize GLAD" << std::endl;
 		return -1;
 	}
 
+    // glEnable(GL_DEBUG_OUTPUT);
+
     // WIP initializeCanvas()...
 
-    // create vertex buffer object (used to store vertices on GPU)
+    std::cout << "Loading shaders..."  << std::endl;
+    Shader canvasShader("src/shader.vs", "src/shader.fs");
+
     // float vertices[] = {
-    //     -1.0f, 1.0f, 0.0f,
-    //     1.0f, 1.0f, 0.0f,
-    //     1.0f, -1.0f, 0.0f,
-    //     -1.0f, -1.0f, 0.0f,
+    //     // positions          // colors           // texture coords
+    //     1.0f,  1.0f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // top right
+    //     1.0f, -1.0f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // bottom right
+    //     -1.0f, -1.0f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // bottom left
+    //     -1.0f,  1.0f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // top left 
     // };
-    // unsigned int VBO;
-    // glGenBuffers(1, &VBO);
 
-    // // bind to GL_ARRAY_BUFFER
-    // glBindBuffer(GL_ARRAY_BUFFER, VBO);
-
-    // // copy canvas vertex data to the buffer (using dynamic draw as we will be updating the texture frequently)
-    // glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_DYNAMIC_DRAW);
-
-    // create the canvas vertex shader object
-    unsigned int vertexShader;
-    vertexShader = glCreateShader(GL_VERTEX_SHADER);
-
-    // compile the canvas vertex shader
-    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-    glCompileShader(vertexShader);
-
-    // check that shader compiles successfully
-    int success;
-    char infoLog[512];
-    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-    if (!success)
-    {
-        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
-    }
-
-    // create the canvas fragment shader object (used to color pixels, this is where main sand logic will go?)
-    unsigned int fragmentShader;
-    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-    glCompileShader(fragmentShader);
-
-    // check that fragment shader compiles successfully
-    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
-    if (!success)
-    {
-        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
-    }
-
-    // link fragment and vertex shaders to create shader program
-    unsigned int shaderProgram;
-    shaderProgram = glCreateProgram();
-    glAttachShader(shaderProgram, vertexShader);
-    glAttachShader(shaderProgram, fragmentShader);
-    glLinkProgram(shaderProgram);
-
-    // verify that the shader program compiled correctly
-    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
-    if(!success)
-    {
-        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
-    }
-
-    // use the program we just constructed and clean up the shader objects
-    glUseProgram(shaderProgram);
-    glDeleteShader(vertexShader);
-    glDeleteShader(fragmentShader);
-
-    // use elemental buffer object for rectangle shaped canvas
     float vertices[] = {
-        -1.0f, 1.0f, 0.0f, // top left
-        1.0f, 1.0f, 0.0f, // top right
-        1.0f, -1.0f, 0.0f, // bottom right
-        -1.0f, -1.0f, 0.0f, // bottom left
+        // positions          // colors           // texture coords
+        0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // top right
+        0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // bottom right
+        -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // bottom left
+        -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // top left 
     };
-    unsigned int indices[] = {
-        1, 2, 0, // first triangle
-        2, 3, 0  // second triangle
+    unsigned int indices[] = {  
+        0, 1, 3, // first triangle
+        1, 2, 3  // second triangle
     };
+    // unsigned int indices[] = {
+    //     1, 2, 0, // first triangle
+    //     2, 3, 0  // second triangle
+    // };
 
     // create vertex buffer object (used to store vertices on GPU), vertex array object, and elemental buffer object (create rectangle from two triangles)
     // VAO is required for Core OpenGL
+    std::cout << "Creating VAO, VBO, and EBO..."  << std::endl;
     unsigned int VAO, VBO, EBO;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
     glGenBuffers(1, &EBO);
+
     glBindVertexArray(VAO); 
 
     // bind VBO and EBO to buffers
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-
     // copy canvas vertex data to the buffer (using dynamic draw as we will be updating the texture frequently)
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_DYNAMIC_DRAW);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_DYNAMIC_DRAW);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    // glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_DYNAMIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+    // glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_DYNAMIC_DRAW);
 
     // instruct OpenGL on how to interpret the vertex data 
     // 3 * sizeof(float) is the stride, may need to tweak (stride is space between  vertex attributes)
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    // position attributes
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
 
+    // color attributes
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3* sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+    // texture coord attribute
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+    glEnableVertexAttribArray(2);
+
     // unbind buffer now that glVertexAttribPointer registered VBO as the vertex attribute's bound VBO
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    // glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    unsigned int texture1;
+    // glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+    glGenTextures(1, &texture1);
+    // glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, texture1);
+    // glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    // glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    // glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    // glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+
+    std::cout << "Generating canvas..."  << std::endl;
+    std::cout << "Height: "  << SCR_HEIGHT << std::endl;
+    std::cout << "Width: "  << SCR_WIDTH << std::endl;
+
+    // create initial canvas array
+    // unsigned char canvasData[SCR_HEIGHT/2][SCR_WIDTH/2][3];
+    // GLubyte canvasData[SCR_HEIGHT][SCR_WIDTH][4];
+    // GLubyte canvasData[64][64][3];
+    // int value = 0;
+    // for(int row = 0; row < 64; row++) {
+    //     for(int col = 0; col < 64; col++) {
+    //         // std::cout << "Row: " << row << std::endl;
+    //         // std::cout << "Col: " << col << std::endl;
+    //         value = (((row & 0x8) == 0) ^ ((col & 0x8) == 0)) * 255;
+    //         // std::cout << "Value: " << value << std::endl;
+    //         canvasData[row][col][0] = (GLubyte)value;
+    //         canvasData[row][col][1] = (GLubyte)value;
+    //         canvasData[row][col][2] = (GLubyte)value;
+    //         // canvasData[row][col][3] = (GLubyte)255;
+
+    //         // canvasData[row][col][0] = (GLubyte)(value%255);
+    //         // canvasData[row][col][1] = (GLubyte)((value*row)%255);
+    //         // canvasData[row][col][2] = (GLubyte)((value*col)%255);
+    //         // value++;
+    //         // canvasData[row][col][0] = (unsigned char)100;
+    //         // canvasData[row][col][1] = (unsigned char)100;
+    //         // canvasData[row][col][2] = (unsigned char)100;
+    //     }
+    // }
+    // load image, create texture and generate mipmaps
+    int width, height, nrChannels;
+    stbi_set_flip_vertically_on_load(true); // tell stb_image.h to flip loaded texture's on the y-axis.
+    // The FileSystem::getPath(...) is part of the GitHub repository so we can find files on any IDE/platform; replace it with your own image path.
+    unsigned char *data = stbi_load("src/container.jpg", &width, &height, &nrChannels, 0);
+    if (data)
+    {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else
+    {
+        std::cout << "Failed to load texture" << std::endl;
+    }
+    std::cout << "Finished generating canvas..."  << std::endl;
+    std::cout << glGetError() << std::endl;
+    // glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, 64, 64, 0, GL_RGB, GL_UNSIGNED_INT, canvasData);
+    std::cout << "Texture initialized..."  << std::endl;
+
+    canvasShader.use();
+    glUniform1i(glGetUniformLocation(canvasShader.ID, "texture1"), 0);
 
     // initialize canvas
     // initializeCanvas();
@@ -174,18 +205,25 @@ int main()
 		processInput(window);
 
 		// render
-		// glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-		// glClear(GL_COLOR_BUFFER_BIT);
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
 
-        glUseProgram(shaderProgram);
+        // bind texture
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, texture1);
+
+        canvasShader.use();
         glBindVertexArray(VAO);
-        // glDrawArrays(GL_TRIANGLES, 0, 3);
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
 		// glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
+
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
 	// glfw: terminate, clearing all previously allocated GLFWresources.
 	//---------------------------------------------------------------
 	glfwTerminate();
