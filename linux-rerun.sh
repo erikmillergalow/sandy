46a05b4a32cff47f4cbdd870e92b95e8a0da82ae
Erik Miller-Galow <erikmillergalow@gmail.com>
2024-08-30 01:54:34 -0400

    Basic colorful window

--- linux-rerun.sh
+++ linux-rerun.sh
@@ -1,6 +1,7 @@
-# sandy
+#!/bin/bash
 
-### build
-```shell
+set -x
+
+rm sandy
 gcc main.c -o sandy -Iinclude -lGL -ldl -lm -lX11 -lasound -lXi -lXcursor
-```
+./sandy
---
+++ main.c
@@ -0,0 +1,62 @@
+#define SOKOL_IMPL
+#define SOKOL_APP_IMPL
+#define SOKOL_GLUE_IMPL
+#define SOKOL_GLCORE
+
+#include "sokol_gfx.h"
+#include "sokol_app.h"
+#include "sokol_glue.h"
+#include "sokol_log.h"
+
+sg_pass_action pass_action;
+
+// app state
+/* static struct { */
+/*     sg_pipeline pip; */
+/*     sg_bindings bind; */
+/*     sg_pass_action pass_action; */
+/*     uint8_t file_buffer; */
+/* } */
+
+void init(void) {
+    sg_setup(&(sg_desc){
+        .environment = sglue_environment(),
+        .logger.func = slog_func,
+    });
+
+    pass_action = (sg_pass_action) {
+        .colors[0] = {
+            .load_action = SG_LOADACTION_CLEAR,
+            .clear_value = { 1.0f, 0.0f, 0.0f, 1.0f }
+        }
+    };
+}
+
+void frame(void) {
+    float g = pass_action.colors[0].clear_value.g + 0.01f;
+    pass_action.colors[0].clear_value.g = (g > 1.0f) ? 0.0f : g;
+    sg_begin_pass(&(sg_pass){ .action = pass_action, .swapchain = sglue_swapchain() });
+    /* __dbgui_draw(); */
+    sg_end_pass();
+    sg_commit();
+}
+
+void cleanup(void) {
+    /* __dbgui_shutdown(); */
+    sg_shutdown();
+}
+
+sapp_desc sokol_main(int argc, char* argv[]) {
+    (void)argc;
+    (void)argv;
+    return (sapp_desc){
+        .init_cb = init,
+        .frame_cb = frame,
+        .cleanup_cb = cleanup,
+        /* event_cb = __dbgui_event, */
+        .width = 800,
+        .height = 600,
+        .window_title = "sandy",
+        .logger.func = slog_func,
+    };
+}
