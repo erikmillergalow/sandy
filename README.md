69ae0b5eb94d35477266e653afcecdef48ef36a8
Erik Miller-Galow <erikmillergalow@gmail.com>
2024-08-31 01:00:13 -0400

    Drawable materials, sokol texture rendering with pixel buffer, left/right click mouse inputs

--- README.md
+++ README.md
@@ -2,5 +2,7 @@
 
 ### build
 ```shell
+../sokol-tools-bin/bin/linux/sokol-shdc -i basic.glsl -o basic.glsl.h -l glsl430
+
 gcc main.c -o sandy -Iinclude -lGL -ldl -lm -lX11 -lasound -lXi -lXcursor
 ```
--- main.c
+++ main.c
@@ -3,20 +3,88 @@
 #define SOKOL_APP_IMPL
 #define SOKOL_GLUE_IMPL
 #define SOKOL_GLCORE
+#define SOKOL_DEBUG
 
 #include "sokol_gfx.h"
 #include "sokol_app.h"
 #include "sokol_glue.h"
 #include "sokol_log.h"
+#include "basic.glsl.h"
+
+// nice that we can define shaders inline - uncertain if this works
+// across all platforms
+
+// vertex Shader
+static const char* vs_source = 
+    "#version 330\n"
+    "in vec3 position;\n"
+    "in vec3 aColor;\n"
+    "in vec2 aTexCoord;\n"
+    "\n"
+    "out vec3 ourColor;\n"
+    "out vec2 TexCoord;\n"
+    "\n"
+    "void main() {\n"
+    "    gl_Position = vec4(position, 1.0);\n"
+    "    ourColor = aColor;\n"
+    "    TexCoord = aTexCoord;\n"
+    "}\n";
+
+// fragment Shader
+static const char* fs_source = 
+    "#version 330\n"
+    "out vec4 FragColor;\n"
+    "\n"
+    "in vec3 ourColor;\n"
+    "in vec2 TexCoord;\n"
+    "\n"
+    "uniform sampler2D ourTexture;\n"
+    "\n"
+    "void main() {\n"
+    "    FragColor = texture(ourTexture, TexCoord);\n"
+    "}\n";
 
 sg_pass_action pass_action;
+/*
+    sg_bindings
+
+    The sg_bindings structure defines the resource binding slots
+    of the sokol_gfx render pipeline, used as argument to the
+    sg_apply_bindings() function.
+
+    A resource binding struct contains:
+
+    - 1..N vertex buffers
+    - 0..N vertex buffer offsets
+    - 0..1 index buffers
+    - 0..1 index buffer offsets
+    - 0..N vertex shader stage images
+    - 0..N vertex shader stage samplers
+    - 0..N vertex shader storage buffers
+    - 0..N fragment shader stage images
+    - 0..N fragment shader stage samplers
+    - 0..N fragment shader storage buffers
+
+    For the max number of bindings, see the constant definitions:
+
+    - SG_MAX_VERTEX_BUFFERS
+    - SG_MAX_SHADERSTAGE_IMAGES
+    - SG_MAX_SHADERSTAGE_SAMPLERS
+    - SG_MAX_SHADERSTAGE_STORAGEBUFFERS
+
+    The optional buffer offsets can be used to put different unrelated
+    chunks of vertex- and/or index-data into the same buffer objects.
+*/
+sg_bindings bind;
+sg_pipeline pip;
 
 size_t canvas_width = 800;
 size_t canvas_height = 600;
 
-size_t *canvas;
-size_t *nextCanvas;
-size_t *updated;
+int *canvas; // current world state
+int *next_canvas; // next world state
+int *updated; // track updated cells
+uint8_t *world_pixels; // colors to send to texture for rendering
 
 enum {
     EMPTY,
@@ -25,8 +93,48 @@ enum {
     WATER,
 };
 
+float draw_position[2];
 bool drawPrimary = false;
 bool drawSecondary = false;
+int primaryMaterial = SAND;
+int secondaryMaterial = WATER;
+int placement_radius = 10;
+
+void place_material(int material, int radius) {
+    for (int i = -placement_radius / 2; i < placement_radius / 2; i++) {
+        size_t x = draw_position[0] + i;
+        for (int j = -placement_radius / 2; j < placement_radius / 2; j++) {
+            size_t y = canvas_height - draw_position[1] + j;
+            if (x < canvas_width && y < canvas_height) {
+                int index = x + canvas_width * y;
+                canvas[index] = material;
+            }
+        }
+    }
+}
+
+void draw_material(int index, int material) {
+    if (material == WALL) {
+        world_pixels[index] = 170;
+        world_pixels[index + 1] = 170;
+        world_pixels[index + 2] = 170;
+        world_pixels[index + 3] = 255;
+    } else if (material == SAND) {
+        world_pixels[index] = 240;
+        world_pixels[index + 1] = 200;
+        world_pixels[index + 2] = 60;
+        world_pixels[index + 3] = 255;
+    } else if (material == WATER) {
+        world_pixels[index] = 30;
+        world_pixels[index + 1] = 90;
+        world_pixels[index + 2] = 220;
+        world_pixels[index + 3] = 255;
+    }
+}
+
+
+sg_image_desc canvas_desc;
+sg_image canvas_texture;
 
 void init(void) {
     sg_setup(&(sg_desc){
@@ -34,23 +142,131 @@ void init(void) {
         .logger.func = slog_func,
     });
 
-    pass_action = (sg_pass_action) {
-        .colors[0] = {
-            .load_action = SG_LOADACTION_CLEAR,
-            .clear_value = { 1.0f, 0.0f, 0.0f, 1.0f }
-        }
+    // bind texture vertices 
+    float vertices[] = {
+        // positions         // colors           // texture coords
+        1.0f,  1.0f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // top right
+        1.0f, -1.0f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // bottom right
+        -1.0f, -1.0f, 0.0f,  0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // bottom left
+        -1.0f,  1.0f, 0.0f,  1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // top left 
     };
+    bind.vertex_buffers[0] = sg_make_buffer(&(sg_buffer_desc){
+        .size = sizeof(vertices),
+        .data = SG_RANGE(vertices),
+        .label = "quad-vertices"
+    });
+
+    // bind texture indices
+    uint16_t indices[] = {
+        0, 1, 3, // first triangle
+        1, 2, 3  // second triangle
+    };
+    bind.index_buffer = sg_make_buffer(&(sg_buffer_desc){
+        .type = SG_BUFFERTYPE_INDEXBUFFER,
+        .size = sizeof(indices),
+        .data = SG_RANGE(indices),
+        .label = "quad-indices"
+    });
+
+    /* sg_shader shd = sg_make_shader(simple_shader_desc(sg_query_backend())); */
+    sg_shader_desc shader_desc = {
+        .vs.source = vs_source,
+        .fs.source = fs_source,
+        .fs.images[0] = { .image_type = SG_IMAGETYPE_2D }
+    };
+    sg_shader shader = sg_make_shader(&shader_desc);
+    pip = sg_make_pipeline(&(sg_pipeline_desc){
+        .shader = shader,
+        .index_type = SG_INDEXTYPE_UINT16,
+        .layout = {
+            .attrs = {
+                [0].format = SG_VERTEXFORMAT_FLOAT3,
+                [1].format = SG_VERTEXFORMAT_FLOAT3,
+                [2].format = SG_VERTEXFORMAT_FLOAT2,
+            }
+        },
+        .label = "triangle-pipeline"
+    });
+
+    // not completely sure why this isn't needed anymore, must be something
+    // to do with simple_shader_desc not setting .fs.images[0].image_type?
+    //
+    // fs stands for fragment shader, NOT file system
+    // bind texture to fragment shader, set sampling options
+    /* bind.fs.images[0] = canvas_texture; */
+    /* bind.fs.samplers[0] = sg_make_sampler(&(sg_sampler_desc){ */
+    /*     .min_filter = SG_FILTER_LINEAR, */
+    /*     .mag_filter = SG_FILTER_LINEAR, */
+    /*     .wrap_u = SG_WRAP_CLAMP_TO_EDGE, */
+    /*     .wrap_v = SG_WRAP_CLAMP_TO_EDGE */
+    /* }); */
+
+    // clear framebuffer
+    pass_action = (sg_pass_action) {
+        .colors[0] = { .load_action=SG_LOADACTION_CLEAR, .clear_value={0.0f, 0.0f, 0.0f, 1.0f} }
+    };
+
+    // initialize world
+    canvas = malloc(canvas_width * canvas_height * sizeof(int));
+    next_canvas = malloc(canvas_width * canvas_height * sizeof(int));
+    updated = malloc(canvas_width * canvas_height * sizeof(int));
+    world_pixels = malloc(canvas_width * canvas_height * 4 * sizeof(uint8_t));
+    for (size_t i = 0; i < canvas_width * canvas_height; i++) {
+        canvas[i] = 0;
+        next_canvas[i] = 0;
+        updated[i] = 0;
+    }
+    
+    // initialize canvas texture
+    canvas_desc = (sg_image_desc){
+        .width = canvas_width,
+        .height = canvas_height,
+        .pixel_format = SG_PIXELFORMAT_RGBA8,
+        .usage = SG_USAGE_STREAM
+    };
+    canvas_texture = sg_make_image(&canvas_desc);
+    if (!canvas_texture.id) {
+        fprintf(stderr, "failed to create canvas texture\n");
+    }
 }
 
 void frame(void) {
-    float g = pass_action.colors[0].clear_value.g + 0.01f;
-    pass_action.colors[0].clear_value.g = (g > 1.0f) ? 0.0f : g;
 
-    // update canvas
+    // handle user input
+    if (drawPrimary) {
+        place_material(primaryMaterial, placement_radius);
+    } else if (drawSecondary) {
+        place_material(secondaryMaterial, placement_radius);
+    }
 
+    // run simulation frame
+    //
+
+    size_t index = 0;
+    for (size_t i = 0; i < canvas_width * canvas_height; i++) {
+        draw_material(index, canvas[i]);
+        /* world_pixels[index] = (255 + i) % 255; */
+        /* world_pixels[index + 1] = i % 255; */
+        /* world_pixels[index + 2] = (i + index) % 255; */
+        /* world_pixels[index + 3] = 255; */
+        index += 4;
+    } 
+
+    // send pixel color buffer to texture to render
+    sg_image_data canvas_update = {
+        .subimage[0][0] = {
+            .ptr = world_pixels,
+            .size = canvas_width * canvas_height * 4
+        }
+    };
+    sg_update_image(canvas_texture, &canvas_update);
 
     // passes render into swapchain, presenting rendering result onto the screen
     sg_begin_pass(&(sg_pass){ .action = pass_action, .swapchain = sglue_swapchain() });
+
+    sg_apply_pipeline(pip);
+    sg_apply_bindings(&bind);
+    sg_draw(0, 6, 1);
     
     /* __dbgui_draw(); */
     
@@ -63,6 +279,9 @@ void frame(void) {
 
 void cleanup(void) {
     /* __dbgui_shutdown(); */
+    free(canvas);
+    free(next_canvas);
+    free(updated);
     sg_shutdown();
 }
 
@@ -71,6 +290,13 @@ void event(const sapp_event* e) {
         if (e->key_code == SAPP_KEYCODE_ESCAPE) {
             sapp_request_quit();
         }
+    }
+
+    if (e->type == SAPP_EVENTTYPE_MOUSE_MOVE) {
+        draw_position[0] = e->mouse_x;
+        draw_position[1] = e->mouse_y;
+        /* printf("x: %f\n", e->mouse_x); */
+        /* printf("y: %f\n", e->mouse_y); */
     }
 
     if (e->type == SAPP_EVENTTYPE_MOUSE_DOWN) {
